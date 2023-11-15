d4f7912f7bda272d005ad6c96ca3bbf5bf61e9ef
JannKGK <jannkgk@gmail.com>
2023-11-15 19:31:15 +0200

    Add Car model, CarDto and car services

---
+++ TARpe21ShopVaitmaa.Core/Dto/CarDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TARpe21ShopVaitmaa.Core.Domain;
+
+namespace TARpe21ShopVaitmaa.Core.Dto
+{
+    public class CarDto
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool IsUsed { get; set; }
+
+        public IEnumerable<FileToApi> FilesToApi { get; set; } = new List<FileToApi>(); //files to be added to the api
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
