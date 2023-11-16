20462ba757436eadd4c0745ec8d67060fd1acd86
Kristjan-kessel <no>
2023-11-16 10:11:21 +0200

    Weather models

---
+++ TARpe21ShopVaitmaa.ApplicationServices/Services/WeatherForecastsServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpe21ShopVaitmaa.ApplicationServices.Services
+{
+    public class WeatherForecastsServices
+    {
+
+    }
+
+}
